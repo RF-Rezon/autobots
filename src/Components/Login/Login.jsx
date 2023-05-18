6d372d75cfde1ee8c561277b9b38febafc6dc0d1
RF-Rezon <rezwanfaysalrezon@gmail.com>
2023-05-18 22:23:34 +0600

    login page added

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -14,11 +14,10 @@ const Login = () => {
 <section className="relative flex flex-wrap lg:h-screen lg:items-center rounded-md my-3">
   <div className="w-full px-4 py-12 sm:px-6 sm:py-16 lg:w-1/2 lg:px-8 lg:py-24">
     <div className="mx-auto max-w-lg text-center">
-      <h1 className="text-2xl font-bold sm:text-3xl">Get started today!</h1>
+      <h1 className="text-2xl font-bold sm:text-3xl">Get started again!</h1>
 
       <p className="mt-4 text-gray-500">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Et libero nulla
-        eaque error neque ipsa culpa autem, at itaque nostrum!
+        You came back! We welcome you
       </p>
     </div>
 
@@ -93,7 +92,7 @@ const Login = () => {
       <div className="flex items-center justify-between">
         <p className="text-sm text-gray-500">
           No account?
-          <a className="underline" href="/register">Sign up</a>
+          <a className="underline mx-2" href="/register">Sign up</a>
         </p>
 
         <button
@@ -109,8 +108,8 @@ const Login = () => {
   <div className="relative h-64 w-full sm:h-96 lg:h-full lg:w-1/2">
     <img
       alt="Welcome"
-      src="https://images.unsplash.com/photo-1630450202872-e0829c9d6172?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
-      className="absolute inset-0 h-full w-full object-cover"
+      src="https://i.ibb.co/sJwM7GQ/1.png"
+      className="absolute inset-0 h-full w-9/12 object-cover "
     />
   </div>
 </section>
